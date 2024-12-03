fe1d4bacd2d7f4d08be81e72bb00c901bba1db36
Tessa Murray <tmurray@bsd220.org>
2024-12-03 07:49:13 -0800

    Initial Commit

---
+++ Playlist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Playlist
+//
+//  Created by Tessa Murray on 12/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Playlist/PlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistApp.swift
+//  Playlist
+//
+//  Created by Tessa Murray on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
